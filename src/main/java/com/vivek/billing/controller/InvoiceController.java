782b8e2500fef64240436fcf24b9f5d6bcd6590d
Aditya Kumar <singhaaditya1996@gmail.com>
2026-05-09 05:04:32 +0530

    Optimized PDF generation speed

--- src/main/java/com/vivek/billing/controller/InvoiceController.java
+++ src/main/java/com/vivek/billing/controller/InvoiceController.java
@@ -1,7 +1,6 @@
 package com.vivek.billing.controller;
 
 import com.vivek.billing.entity.Invoice;
-import com.vivek.billing.service.EmailService;
 import com.vivek.billing.service.InvoiceService;
 import com.vivek.billing.service.PdfService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +18,6 @@ public class InvoiceController {
     @Autowired
     private PdfService pdfService;
 
-    @Autowired
-    private EmailService emailService;
-
     // CREATE INVOICE
     @PostMapping
     public Invoice createInvoice(
@@ -31,39 +27,6 @@ public class InvoiceController {
         // SAVE INVOICE
         Invoice savedInvoice =
                 invoiceService.createInvoice(invoice);
-
-        // GENERATE PDF
-        byte[] pdfBytes =
-                pdfService.generatePdf(savedInvoice);
-
-        // SEND EMAIL ONLY IF EMAIL EXISTS
-        if (savedInvoice.getCustomerEmail() != null
-                &&
-                !savedInvoice.getCustomerEmail().trim().isEmpty()) {
-
-            try {
-
-                emailService.sendInvoiceEmail(
-                        savedInvoice,
-                        pdfBytes
-                );
-
-                System.out.println(
-                        "Invoice email sent successfully"
-                );
-
-            } catch (Exception e) {
-
-                // IMPORTANT:
-                // EVEN IF EMAIL FAILS,
-                // BILL GENERATION SHOULD CONTINUE
-
-                System.out.println(
-                        "Email sending failed : "
-                                + e.getMessage()
-                );
-            }
-        }
 
         // RETURN SAVED INVOICE
         return savedInvoice;
@@ -75,12 +38,15 @@ public class InvoiceController {
             @PathVariable Long id
     ) {
 
+        // GET INVOICE
         Invoice invoice =
                 invoiceService.getById(id);
 
+        // GENERATE PDF
         byte[] pdf =
                 pdfService.generatePdf(invoice);
 
+        // RETURN PDF
         return ResponseEntity.ok()
                 .header(
                         "Content-Disposition",
